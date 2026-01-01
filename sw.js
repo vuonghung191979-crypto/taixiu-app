a7984c8ce44cb4276eba89cd4d9da4e3b3c0ece6
vuonghung191979-crypto <vuonghung191979@icloud.com>
2026-01-01 13:43:09 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME="taixiu-cache";
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["index.html"])));
-});
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
